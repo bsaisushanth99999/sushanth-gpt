b0114a853e0a3e08a22f41276c809047ee962964
BynagariSaiSushanthReddy <bsaisushanth@gmail.com>
2023-10-09 11:14:56 +0530

    auto scroll addeed

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -3,7 +3,7 @@
 import { db } from "@/fireBase";
 import { DocumentData, collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { CollectionHook, useCollection } from "react-firebase-hooks/firestore";
 import Message from "./Message";
 
@@ -29,8 +29,23 @@ function Chat({ chatId }: Props) {
       )
   );
 
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+
+  // Function to scroll to the bottom of the chat container
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    // Scroll to the bottom when new messages arrive
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <div className="flex-1 overflow-y-auto">
+    <div className="flex-1 h-screen overflow-y-auto" ref={chatContainerRef}>
       {messages?.docs.map((message) => (
         <Message key={message.id} message={message.data()} />
       ))}
